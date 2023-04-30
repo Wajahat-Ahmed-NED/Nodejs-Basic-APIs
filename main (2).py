3b1ac19abb05af09f954360c7ace829f7ff22452
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-05-01 03:53:09 +0500

    update

--- main (2).py
+++
@@ -1,9 +0,0 @@
-from gophish import Gophish
-print("Hello World")
-
-api_key = 'fd8bc15c96d3809f1b44d852936cb71394836bbf411b4d67321a3508d02862f6'
-api = Gophish(api_key, host='http://localhost:3333', verify=False)
-for campaign in api.campaigns.get():
-    print (campaign.name)
-# print(len(campaigns))
-# print(compaigns)
